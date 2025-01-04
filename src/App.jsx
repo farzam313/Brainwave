e9807388b6ca98986541ffa4e5dd099e70a45239
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-01-05 04:03:31 +0530

    Creating Header and placed BrainWave logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="pt-[4.7rem] lg:pt-[5.2rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something...
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
