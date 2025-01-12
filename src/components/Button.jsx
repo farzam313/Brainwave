ba041f26f57a27ff6c7fefb145abc7489101bb1c
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-01-13 01:28:35 +0530

    Mobile menue added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ classname, href, onClick, children, px, white }) => {
-  const classes = ` button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
-  }  
-    ${white ? "text-n-8" : "text-n-1"} ${classname || ""}`;
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
+
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
@@ -19,6 +19,7 @@ const Button = ({ classname, href, onClick, children, px, white }) => {
       {ButtonSvg(white)}
     </a>
   );
+
   return href ? renderLink() : renderButton();
 };
 
