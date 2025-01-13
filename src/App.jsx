67abd8dc0e86e67f42dd0db3aac5078d1101d319
farzam313 <107337733+farzam313@users.noreply.github.com>
2025-01-14 03:48:42 +0530

    Skleton and Hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.7rem] lg:pt-[5.2rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
